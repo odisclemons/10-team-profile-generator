733beb25e9aeb1eb6cac6f07400a339fb4a6e0c1
Odis Clemons <odis.clemons@gmail.com>
2022-04-28 16:11:48 -0400

    moved template and css to a template folder

---
+++ script.js
@@ -0,0 +1,14 @@
+const cardTemplate = `
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title">${name}<br />Manager</h5>
+        <div class="card-bottom">
+            <p>ID: ${id}</p>
+            <p>Email: <a href="mailto:${email}">${email}</a></p>
+            <p>Office number: ${officeNumber}</p>
+        </div>
+    </div>
+</div>`
+
+
+
