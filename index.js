83fd5513ce60187248a6bd4faafdd7e64ba92f85
Odis Clemons <odis.clemons@gmail.com>
2022-04-28 19:58:06 -0400

    added conditional questions for school and github

--- index.js
+++ index.js
@@ -6,20 +6,25 @@ const templateFilePath = path.join(__dirname, 'template.html');
 const finalHtmlPath = path.join(__dirname, 'profile.html');
 
 var team = [];
-var positions = ["manager", "engineer", "member"];
+var icons = { manager: '<i class="fas fa-mug-hot"></i>', engineer: '<i class="fas fa-glasses"></i>', intern: '<i class="fas fa-user-graduate"></i>' }
+
+// 1-liner to capitalize words
+const capitalize = (word) => word.charAt(0).toUpperCase() + word.slice(1)
 
 const seperator = () => console.log("=========================================================")
 
 // returns the html card with each of the provided details filled in
-const cardTemplate = ({ position, name, id, email, officeNumber }) => `
+const cardTemplate = ({ position, name, id, email, officeNumber, github, school }) => `
 
 <div class="card">
     <div class="card-body">
-        <h5 class="card-title">${name}<br />${position}</h5>
+        <h5 class="card-title">${name}<br />${icons[position] + capitalize(position)}</h5>
         <div class="card-bottom">
             <p>ID: ${id}</p>
             <p>Email: <a href="mailto:${email}">${email}</a></p>
-            <p>Office number: ${officeNumber}</p>
+            ${position === 'manager' ? `<p>Office number: ${officeNumber}</p>` : ''}
+            ${position === 'engineer' ? `<p>Github: <a href="https://github.com/${github}" target="_blank">${github}</a></p>` : ''}
+            ${position === 'intern' ? `<p>School: <a href="https://google.com/search?q=${school.replace(" ", "+")} target="_blank">${school}</a></p>` : ''}
         </div>
     </div>
 </div>
@@ -28,62 +33,92 @@ const cardTemplate = ({ position, name, id, email, officeNumber }) => `
 // function to return array of questions. 
 // dynamically insert the position of the team mamber as "position"
 const questions = (position) => {
-    let tempQuestions =
+
+    // different questions need to be asked to managers vs everybody else
+    const commonQuestions = [
+        {
+            type: 'input',
+            name: 'name',
+            message: `Enter ${position === 'manager' ? 'manager' : 'team member'}'s full name.`,
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: `Enter employee ID.`,
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: `Enter email address.`,
+        },
+    ]
+
+    const onlyManagers = [{
+        type: 'input',
+        name: 'officeNumber',
+        message: `Enter office number.`,
+    }
+    ]
+
+    const everybodyElse =
         [
             {
-                type: 'input',
-                name: 'name',
-                message: `Enter team ${position}'s full name.`,
+                type: 'list',
+                name: 'position',
+                message: `Team member's position?`,
+                choices: [{ name: "engineer", value: "engineer" }, { name: "intern", value: "intern" }]
             },
             {
                 type: 'input',
-                name: 'id',
-                message: `Enter team ${position}'s employee ID.`,
+                name: 'github',
+                message: `Github username.`,
+                when(answers) {
+                    return answers.position === 'engineer'
+                }
             },
             {
                 type: 'input',
-                name: 'email',
-                message: `Enter team ${position}'s email address.`,
+                name: 'school',
+                message: `School name`,
+                when(answers) {
+                    return answers.position === 'intern'
+                }
             },
             {
-                type: 'input',
-                name: 'number',
-                message: `Enter team ${position}'s office number.`,
-            },
-
+                type: 'confirm',
+                name: 'done',
+                message: "Done?",
+                default: false
+            }
         ]
 
-    // only provide the last question if we're asking about team members
-    // this way it will keep looping until there are no team members left to add
-    let done = {
-        type: 'confirm',
-        name: 'done',
-        message: "Done?",
-        default: false
-    }
-
-    return position !== 'member' ? tempQuestions : [...tempQuestions, done]
+    // if user is manager give them common questions and onlyMangers 
+    // otherwise give them the everybodyElse array 
+    return position === 'manager' ? [...commonQuestions, ...onlyManagers] : [...commonQuestions, ...everybodyElse]
 }
 
 
 async function init() {
     var i = 0;
-    var position;
 
     var done = false;
 
     // loop through and keep asking them details until they tell us stop
     // p.s. I just realized inquirer can loop questions.  DOH!
     do {
-        // if position is manager or engineer, insert that as their position
-        // else, insert "member" for any time we go past the first 2 
-        position = i < 2 ? positions[i] : positions[2]
+        // the first time, we pass 'manager' as position.
+        // after that we can loop and ask what type of team member until theyre done
 
         // await the answers from their prompts
-        let answers = await inq.prompt(questions(position))
+        // on the first loop, pass manager as position.
+        let answers = await inq.prompt(questions(i === 0 ? 'manager' : ''))
+
+        // if they're a manager, add this key manually since it wont be set by inquirer
+        if (i === 0) answers.position = 'manager';
         done = answers.done
-        //append their team position to their answers object
-        team.push({ ...answers, position })
+
+        //add them to the list
+        team.push(answers)
         i++
         seperator()
     } while (!done)
@@ -100,7 +135,13 @@ const generatePage = async () => {
     seperator()
 
     // load contents of the template file
-    let finalHtml = fs.readFileSync(templateFilePath)
+    try {
+        var finalHtml = await fs.readFileSync(templateFilePath, 'utf-8')
+    }
+    catch (err) {
+        console.error("Error reading from file: " + err)
+        process.exit()
+    }
 
     // replace that comented text with the entire string of generated cards
     finalHtml = finalHtml.replace('<!-- cards go here -->', finalTeam)
@@ -109,6 +150,7 @@ const generatePage = async () => {
     fs.writeFile(finalHtmlPath, finalHtml, (err) => {
         if (err) {
             console.error("Error writing file: " + err)
+
             return;
         }
 
