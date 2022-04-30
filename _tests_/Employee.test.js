78996401d46d461d6dd1e842c78c77efe02881fc
Odis Clemons <odis.clemons@gmail.com>
2022-04-30 19:19:55 -0400

    wrote tests for Employee class

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,11 +1,30 @@
 const Employee = require('../lib/employee.js');
 
+var tempEmployee = new Employee('Jane Doe', '1', "test@test.com");
+
 describe("Employee", () => {
     describe("getFullName", () => {
         it("should return the fullName property", () => {
-            var tempEmployee = new Employee('Jane Doe', '1', "test@test.com");
-
             expect(tempEmployee.fullName).toEqual("Jane Doe")
         })
     })
+
+    describe("getId", () => {
+        it("should return the id property", () => {
+            expect(tempEmployee.id).toEqual("1")
+        })
+    })
+
+    describe("getEmail", () => {
+        it("should return the email property", () => {
+            expect(tempEmployee.email).toEqual("test@test.com")
+        })
+    })
+
+    describe("getRole", () => {
+        it("should return the role from the getRole method", () => {
+            expect(tempEmployee.getRole()).toEqual("Employee")
+        })
+    })
+
 })
